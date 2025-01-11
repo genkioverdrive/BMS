ad8f68af922a9c39fed4201098452f1f7e8ed65e
Chetan Verma <vermachetan800@gmail.com>
2025-01-11 08:25:08 +0530

    initial project files, source + header

---
+++ Core/Inc/LTC6804.h
@@ -0,0 +1,13 @@
+/*
+ * LTC6804.h
+ *
+ *  Created on: Jan 11, 2025
+ *      Author: verma
+ */
+
+#ifndef INC_LTC6804_H_
+#define INC_LTC6804_H_
+
+
+
+#endif /* INC_LTC6804_H_ */
---
+++ Core/Src/LTC6804.c
@@ -0,0 +1,9 @@
+/*
+ * LTC6804.c
+ *
+ *  Created on: Jan 11, 2025
+ *      Author: verma
+ */
+
+
+#include "LTC6804.h"
