3a7bc7e04a9c50dd939edabdcbc08c12229297ee
Chetan Verma <vermachetan800@gmail.com>
2025-01-11 08:48:30 +0530

    basic function prototypes added

--- Core/Inc/LTC6804.h
+++ Core/Inc/LTC6804.h
@@ -8,6 +8,35 @@
 #ifndef INC_LTC6804_H_
 #define INC_LTC6804_H_
 
+typedef struct{
+	uint16_t packVoltage;
+	uint8_t SOC;
+}BMSData;
 
+typedef struct{
+	uint16_t slaveID;
+	uint16_t peakTemp;
+	uint16_t lowestTemp;
+	uint16_t highestCellVoltage;
+	uint16_t lowestCellVoltage;
+}BMSBoard;
+
+// Function to check how many cells are present
+uint8_t populateBP(BMSBoard *slaveBoard);
+
+// Get voltage and temperature data from bms slave
+uint16_t getSlaveData(BMSBoard *slaveBoard);
+
+// Get highest cell voltage
+uint16_t getPeakCellVoltage(BMSBoard *slaveBoard);
+
+// Get lowest cell voltage
+uint16_t getLowCellVoltage(BMSBoard *slaveBoard);
+
+// Get highest cell temp
+uint16_t getPeakCellTemp(BMSBoard *slaveBoard);
+
+// Get lowest cell temp
+uint16_t getLowCellTemp(BMSBoard *slaveBoard);
 
 #endif /* INC_LTC6804_H_ */
